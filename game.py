6dd9ed460149996cb01997f2d1ee8c16b4d4e1ee
kaslufl <leallucasfonseca@gmail.com>
2022-04-05 21:06:05 -0300

    fix: rename constants, methods and variables

--- game.py
+++ game.py
@@ -1,111 +1,110 @@
-MINIMAL_POINTS_TO_SCORE_SET = 3
-MINIMAL_SETS_TO_WIN = 4
+MINIMAL_POINTS_TO_ADVANTAGE = 3
+MINIMAL_POINTS_TO_WIN = 4
 MINIMAL_POINTS_DIFERENCE_TO_SCORE_SET = 2
 
 
 class Game:
-    def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
+    def __init__(self, player_1_name, player_2_name):
+        self.player_1_name = player_1_name
+        self.player_2_name = player_2_name
         self.p1points = 0
         self.p2points = 0
 
-    def won_point(self, playerName):
-        if playerName == self.player1Name:
-            self.P1Score()
+    def won_point(self, player_name):
+        if player_name == self.player_1_name:
+            self.player_1_score_point()
         else:
-            self.P2Score()
+            self.player_2_score_point()
 
     def score(self):
         result = ""
         if self.is_game_tied():
             result = self.get_tied_score(self.p1points)
 
-        P1res = ""
-        P2res = ""
+        player_1_score = ""
+        player_2_score = ""
         if (self.p1points > 0 and self.p2points == 0):
             if (self.p1points == 1):
-                P1res = "Fifteen"
+                player_1_score = "Fifteen"
             if (self.p1points == 2):
-                P1res = "Thirty"
+                player_1_score = "Thirty"
             if (self.p1points == 3):
-                P1res = "Forty"
+                player_1_score = "Forty"
 
-            P2res = "Love"
-            result = P1res + "-" + P2res
+            player_2_score = "Love"
+            result = player_1_score + "-" + player_2_score
         if (self.p2points > 0 and self.p1points == 0):
             if (self.p2points == 1):
-                P2res = "Fifteen"
+                player_2_score = "Fifteen"
             if (self.p2points == 2):
-                P2res = "Thirty"
+                player_2_score = "Thirty"
             if (self.p2points == 3):
-                P2res = "Forty"
+                player_2_score = "Forty"
 
-            P1res = "Love"
-            result = P1res + "-" + P2res
+            player_1_score = "Love"
+            result = player_1_score + "-" + player_2_score
 
-        if (self.p1points > self.p2points and self.p1points < MINIMAL_SETS_TO_WIN):
+        if (self.p1points > self.p2points and self.p1points < MINIMAL_POINTS_TO_WIN):
             if (self.p1points == 2):
-                P1res = "Thirty"
+                player_1_score = "Thirty"
             if (self.p1points == 3):
-                P1res = "Forty"
+                player_1_score = "Forty"
             if (self.p2points == 1):
-                P2res = "Fifteen"
+                player_2_score = "Fifteen"
             if (self.p2points == 2):
-                P2res = "Thirty"
-            result = P1res + "-" + P2res
-        if (self.p2points > self.p1points and self.p2points < MINIMAL_SETS_TO_WIN):
+                player_2_score = "Thirty"
+            result = player_1_score + "-" + player_2_score
+        if (self.p2points > self.p1points and self.p2points < MINIMAL_POINTS_TO_WIN):
             if (self.p2points == 2):
-                P2res = "Thirty"
+                player_2_score = "Thirty"
             if (self.p2points == 3):
-                P2res = "Forty"
+                player_2_score = "Forty"
             if (self.p1points == 1):
-                P1res = "Fifteen"
+                player_1_score = "Fifteen"
             if (self.p1points == 2):
-                P1res = "Thirty"
-            result = P1res + "-" + P2res
+                player_1_score = "Thirty"
+            result = player_1_score + "-" + player_2_score
 
-        if (self.p1points > self.p2points and self.p2points >= MINIMAL_POINTS_TO_SCORE_SET):
-            result = "Advantage " + self.player1Name
+        if (self.p1points > self.p2points and self.p2points >= MINIMAL_POINTS_TO_ADVANTAGE):
+            result = "Advantage " + self.player_1_name
 
-        if (self.p2points > self.p1points and self.p1points >= MINIMAL_POINTS_TO_SCORE_SET):
-            result = "Advantage " + self.player2Name
+        if (self.p2points > self.p1points and self.p1points >= MINIMAL_POINTS_TO_ADVANTAGE):
+            result = "Advantage " + self.player_2_name
 
-        if (self.p1points >= MINIMAL_SETS_TO_WIN and self.p2points >= 0 and (self.p1points - self.p2points) >= MINIMAL_POINTS_DIFERENCE_TO_SCORE_SET):
-            result = "Win for " + self.player1Name
-        if (self.p2points >= MINIMAL_SETS_TO_WIN and self.p1points >= 0 and (self.p2points - self.p1points) >= MINIMAL_POINTS_DIFERENCE_TO_SCORE_SET):
-            result = "Win for " + self.player2Name
+        if (self.p1points >= MINIMAL_POINTS_TO_WIN and self.p2points >= 0 and (self.p1points - self.p2points) >= MINIMAL_POINTS_DIFERENCE_TO_SCORE_SET):
+            result = "Win for " + self.player_1_name
+        if (self.p2points >= MINIMAL_POINTS_TO_WIN and self.p1points >= 0 and (self.p2points - self.p1points) >= MINIMAL_POINTS_DIFERENCE_TO_SCORE_SET):
+            result = "Win for " + self.player_2_name
         return result
 
     def is_game_tied(self):
-        return self.p1points == self.p2points and self.p1points < MINIMAL_POINTS_TO_SCORE_SET
+        return self.p1points == self.p2points and self.p1points < MINIMAL_POINTS_TO_ADVANTAGE
 
     def get_tied_score(self, points):
         result = None
-        if points == MINIMAL_POINTS_TO_SCORE_SET:
+        if points >= MINIMAL_POINTS_TO_ADVANTAGE:
             result = "Deuce"
             return result
-        if (points == 0):
+        if points == 0:
             result = "Love"
-        if (points == 1):
+        if points == 1:
             result = "Fifteen"
-        if (points == 2):
+        if points == 2:
             result = "Thirty"
         result += "-All"
 
         return result
 
-
-    def SetP1Score(self, number):
+    def set_player_1_score(self, number):
         for i in range(number):
-            self.P1Score()
+            self.player_1_score_point()
 
-    def SetP2Score(self, number):
+    def set_player_2_score(self, number):
         for i in range(number):
-            self.P2Score()
+            self.player_2_score_point()
 
-    def P1Score(self):
+    def player_1_score_point(self):
         self.p1points += 1
 
-    def P2Score(self):
+    def player_2_score_point(self):
         self.p2points += 1
